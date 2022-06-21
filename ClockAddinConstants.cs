f789f255bc438462f8090dcc51743bc163476f77
Anchit <anchit@beesys.com>
2022-06-21 16:24:47 +0530

    Version 5.5

---
+++ ClockAddinConstants.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beesys.Wasp.AddIn
+{
+    internal class ClockAddinConstants
+    {
+        internal static readonly string MODULENAME = "Clock";
+        internal static readonly string TIME = "TIME";
+        internal static readonly string HOUR = "HOUR";
+        internal static readonly string MINUTE = "MINUTE";
+        internal static readonly string SECOND = "SECOND";
+        internal static readonly string TODAY = "TODAY";
+        internal static readonly string TOMORROW = "TOMORROW";
+        internal static readonly string DEGREE = "DEGREE";
+        internal static readonly string DATASET = "DataSet";
+        internal static readonly string CLOCK = "Clock";
+        internal static readonly string HELPFILEPATH = "\\HostedAssemblies\\ClockAddIn\\Help.txt";
+        internal const string BtnOKtxt = "70050";
+        internal const string BtnCncltxt = "70051";
+        internal const string DESCRIPTION = "Time in selected format is shown in TIME column.Values of Hour,Minute and Second are shown in degrees in data column of grid and can be used to create analog clock.";
+        internal const string lblTime = "70052";
+        internal const string lblTodaysTime = "70053";
+        internal const string lblTomorrowTime = "70054";
+        internal static readonly string VARTODAYDATE = "Today"; 
+        internal static readonly string VARTOMORROWDATE = "Tomorrow"; 
+        internal static readonly string TIMEFORMATE = "Time Format:";
+        internal static readonly string TODAYTIMEFORMATE = "Today's Date Format:";
+        internal static readonly string TOMORROWTIMEFORMATE = "Tomorrow's Date Format:";
+        internal static readonly string OK = "Ok";
+        internal static readonly string CANCEL = "Cancel";
+    }
+}
---
+++ VersionInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Beehive Systems Ltd")]
+[assembly: AssemblyProduct("WASP3D")]
+[assembly: AssemblyCopyright("© Beehive Systems Ltd. All rights reserved.")]
+[assembly: AssemblyInformationalVersion("2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("5.5.21.1550")]
+ 
